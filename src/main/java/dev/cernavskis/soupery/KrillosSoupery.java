80c25b7f5ebbbed4df99949a96ad37c225aa009a
SwanX1 <karliscern@gmail.com>
2021-03-08 23:04:56 +0200

    Setup Workspace

---
+++ src/main/java/dev/cernavskis/soupery/KrillosSoupery.java
@@ -0,0 +1,12 @@
+package dev.cernavskis.soupery;
+
+import net.fabricmc.api.ModInitializer;
+
+public class KrillosSoupery implements ModInitializer {
+	public static final String MODID = "soupery";
+
+	@Override
+	public void onInitialize() {
+		System.out.println("Initialized Krillo's Soupery!");
+	}
+}
